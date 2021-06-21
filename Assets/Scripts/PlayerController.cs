3b8946f5fb0da3bcff00c197c070986f92de94f6
Kyle Powers <kwpowers@gmail.com>
2021-06-21 01:29:50 -0400

    Added animations to Player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,27 +12,59 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _jumpHeight = 15f;
 
+    private bool _flip = false;
+    private bool _jumping = false;
     private float _yVelocity;
     private Vector3 _direction;
     private Vector3 _velocity;
     private CharacterController _controller = null;
+    private Animator _anim = null;
 
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
+        if (_anim == null)
+        {
+            Debug.LogError("Animator is Null");
+        }
     }
 
     void Update()
     {       
         if (_controller.isGrounded == true)
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
             _direction = new Vector3(0, 0, horizontalInput);
             _velocity = _direction * _speed;
 
+            if (_jumping == true)
+            {
+                _jumping = false;
+            }
+
+            if (horizontalInput < 0 && _flip == false)
+            {
+                _flip = true;
+                transform.rotation = Quaternion.Euler(transform.rotation.x, -180f, transform.rotation.z);
+            }
+            else if (horizontalInput > 0 & _flip == true)
+            {
+                _flip = false;
+                transform.rotation = Quaternion.Euler(transform.rotation.x, 0f, transform.rotation.z);
+            }
+
+            if (_anim != null)
+            {
+                _anim.SetFloat("Speed", Mathf.Abs(horizontalInput));
+                _anim.SetBool("Jump", false);
+            }
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                _jumping = true;
                 _yVelocity = _jumpHeight;
+                _anim.SetBool("Jump", true);
             }
         }
         else
